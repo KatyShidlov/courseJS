372337f3a85117f75f322250cb2c97040d2a4bb9
Ekaterina <katyzakh147@gmail.com>
2022-12-20 16:08:04 +0700

    regular_expr

--- src/js/main.js
+++ src/js/main.js
@@ -409,12 +409,16 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function replaceNotDigit(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
 
-        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) { //width.length - 2 - убираем px
+        if (offset == replaceNotDigit(width) * (slides.length - 1)) { //width.length - 2 - убираем px
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += replaceNotDigit(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -431,9 +435,9 @@ window.addEventListener('DOMContentLoaded', () => {
     prev.addEventListener('click', () => {
 
         if (offset == 0) {
-            offset = (+width.slice(0, width.length - 2) * (slides.length - 1));
+            offset = replaceNotDigit(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= replaceNotDigit(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -455,7 +459,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
             slideIndex = slideTo;
 
-            offset = (+width.slice(0, width.length - 2) * (slideTo - 1));
+            offset = replaceNotDigit(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
